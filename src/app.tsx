9a778e665603cb9ef4227ec94b1dcac555fa98a8
hwoodall30 <hwoodall30@gmail.com>
2024-11-24 21:06:46 -0500

    Adds context to app

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,19 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
 import { ScrollYProvider } from "./context/scrollYPosition";
+import { DeviceTypeProvider } from "./context/deviceType";
+import { ThemeProvider } from "./context/theme";
 
 export default function App() {
 	return (
-		<ScrollYProvider throttleTime={100}>
-			<Router root={(props) => <Suspense>{props.children}</Suspense>}>
-				<FileRoutes />
-			</Router>
-		</ScrollYProvider>
+		<ThemeProvider>
+			<DeviceTypeProvider>
+				<ScrollYProvider throttleTime={100}>
+					<Router root={(props) => <Suspense>{props.children}</Suspense>}>
+						<FileRoutes />
+					</Router>
+				</ScrollYProvider>
+			</DeviceTypeProvider>
+		</ThemeProvider>
 	);
 }
